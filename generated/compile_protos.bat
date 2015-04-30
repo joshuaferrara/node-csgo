ed867b7cfd7ff40b88af5ed5bc2aee9bd78d7f34
Ammar Askar <ammar@ammaraskar.com>
2015-04-30 17:59:13 +0500

    Split up proto definitions that we get from upstream to their own folder for better organization

--- generated/compile_protos.bat
+++ generated/compile_protos.bat
@@ -4,5 +4,5 @@ set PROTODIR=D:\GitHub\SteamKit\Resources\Protobufs
 set PROTOBUFS=base_gcmessages gcsdk_gcmessages cstrike15_gcmessages
 
 for %%X in ( %PROTOBUFS% ) do (
-	protoc --descriptor_set_out=%%X.desc --include_imports %%X.proto
+	protoc --descriptor_set_out=%%X.desc --include_imports --proto_path=protos protos\%%X.proto
 )
--- generated/compile_protos.sh
+++ generated/compile_protos.sh
@@ -1,3 +1,3 @@
-protoc --descriptor_set_out=base_gcmessages.desc --include_imports --proto_path=./ ./base_gcmessages.proto
-protoc --descriptor_set_out=gcsdk_gcmessages.desc --include_imports --proto_path=./ ./gcsdk_gcmessages.proto
-protoc --descriptor_set_out=cstrike15_gcmessages.desc --include_imports --proto_path=./ ./cstrike15_gcmessages.proto
+protoc --descriptor_set_out=base_gcmessages.desc --include_imports --proto_path=./protos protos/base_gcmessages.proto
+protoc --descriptor_set_out=gcsdk_gcmessages.desc --include_imports --proto_path=./protos protos/gcsdk_gcmessages.proto
+protoc --descriptor_set_out=cstrike15_gcmessages.desc --include_imports --proto_path=./protos protos/cstrike15_gcmessages.proto
