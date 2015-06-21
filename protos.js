01556aedb06e17b5b50c1c82d15df22e5cdb6e11
thesupremecommander <thesupremecmdr@gmail.com>
2015-06-21 15:32:10 -0400

    Load correct files.

--- protos.js
+++ protos.js
@@ -3,7 +3,7 @@ var Protobuf = require('protobufjs');
 Protobuf.convertFieldsToCamelCase = true;
 
 var builder = Protobuf.newBuilder();
-Protobuf.loadProtoFile(__dirname + "/generated/base_gcmessages.desc", builder);
-Protobuf.loadProtoFile(__dirname + "/generated/gcsdk_gcmessages.desc", builder);
-Protobuf.loadProtoFile(__dirname + "/generated/cstrike15_gcmessages.desc", builder);
+Protobuf.loadProtoFile(__dirname + "/generated/protos/base_gcmessages.proto", builder);
+Protobuf.loadProtoFile(__dirname + "/generated/protos/gcsdk_gcmessages.proto", builder);
+Protobuf.loadProtoFile(__dirname + "/generated/protos/cstrike15_gcmessages.proto", builder);
 module.exports = builder.build();
