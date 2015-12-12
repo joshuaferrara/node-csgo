8101b0533d0ae0f2487ef8732127e3ca13a6d1c6
Josh Ferrara <scjosh2@gmail.com>
2015-12-11 22:30:04 -0800

    Removed old protobufs

--- generated/compile_protos.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set PROTODIR=D:\GitHub\SteamKit\Resources\Protobufs
-set PROTOBUFS=base_gcmessages gcsdk_gcmessages cstrike15_gcmessages
-
-for %%X in ( %PROTOBUFS% ) do (
-	protoc --descriptor_set_out=%%X.desc --include_imports --proto_path=protos protos\%%X.proto
-)
--- generated/compile_protos.sh
+++
@@ -1,3 +0,0 @@
-protoc --descriptor_set_out=base_gcmessages.desc --include_imports --proto_path=./protos protos/base_gcmessages.proto
-protoc --descriptor_set_out=gcsdk_gcmessages.desc --include_imports --proto_path=./protos protos/gcsdk_gcmessages.proto
-protoc --descriptor_set_out=cstrike15_gcmessages.desc --include_imports --proto_path=./protos protos/cstrike15_gcmessages.proto
