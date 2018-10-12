d2d8a5773c46369ae085f15013cb95ec2747a282
Joshua Ferrara <scjosh2@gmail.com>
2018-10-12 09:56:35 -0700

    Added credential grabbing for test script.

--- test/test.js
+++ test/test.js
@@ -1,17 +1,56 @@
-var steam = require('steam'),
-    csgo = require('../'),
-    steamClient = new steam.SteamClient(),
-    steamUser = new steam.SteamUser(steamClient),
-    steamGC = new Steam.SteamGameCoordinator(bot, 730),
-    CSGOCli = new csgo.CSGOClient(steamUser, steamGC, false),
-    should = require('should');
+var Steam = require('steam'),
+    CSGO = require('../'),
+    steamClient = new Steam.SteamClient(),
+    steamUser = new Steam.SteamUser(steamClient),
+    steamGC = new Steam.SteamGameCoordinator(steamUser, 730),
+    CSGOCli = new CSGO.CSGOClient(steamUser, steamGC, false),
+    should = require('should'),
+    crypto = require('crypto');
+
+function MakeSha(bytes) {
+    var hash = crypto.createHash('sha1');
+    hash.update(bytes);
+    return hash.digest();
+}
+
+Steam.servers = require('../example/servers.json');
+
+var connectedToSteam = false;
+function beConnectedToSteam(done) {
+    if (connectedToSteam) return done();
+    setTimeout(() => beConnectedToSteam(done), 1000);
+}
 
 describe('Steam', () => {
+    // timeout(20000);
+
     describe('#connect', () => {
         it('should connect to Steam', (done) => {
             steamClient.connect();
             steamClient.on('connected', () => {
                 done();
+            });
+        });
+    });
+
+    describe('#login', () => {
+        it('should login to Steam', (done) => {
+            before(beConnectedToSteam);
+
+            steamUser.logOn({
+                "account_name": process.env.steam_username,
+                "password": process.env.steam_password,
+                "sha_sentryfile": MakeSha(Buffer.from(process.env.steam_sentry, 'base64')),
+            });
+
+            steamClient.on('logOnResponse', (resp) => {
+                if (resp.eresult == Steam.EResult.OK) {
+                    loggedInToSteam = true;
+                    done();
+                } else {
+                    console.error('Failed to log on to Steam.');
+                    console.error(resp);
+                }
             });
         });
     });
